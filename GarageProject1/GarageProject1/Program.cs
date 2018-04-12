016df49eefd14d9a6d6cb2de7bad1446e60bd920
Yvette85 <y.kamariza@yahoo.com>
2018-04-12 10:29:42 +0200

    changes accepted

---
+++ GarageProject1/GarageProject1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
